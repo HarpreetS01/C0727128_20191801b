180cdd6b80948c97f4da7d25d6fb0f99de94903e
visualstudio <visualstudio@122-PC20>
2019-01-18 21:39:28 -0500

    final commit handline

--- C0727128/Program.cs
+++ C0727128/Program.cs
@@ -16,7 +16,20 @@ namespace C0727128
             StudentInformation _Cena = new StudentInformation("Cena", 3.4, 20);
             StudentInformation _Mark = new StudentInformation("Mark", 3.2, 10);
             StudentInformation _Abhi = new StudentInformation("Abhi", 3.2, 10);
-            Console.ReadLine();
+
+            //create a collection of Studentinformation Records
+            var StudentList = new List<StudentInformation>();
+            StudentList.Add(_Mary);
+            StudentList.Add(_John);
+            StudentList.Add(_Cena);
+            StudentList.Add(_Mark);
+            StudentList.Add(_Abhi);
+
+            foreach (var student in StudentList )
+            {
+                Console.WriteLine(student.Name);
+            }
+                Console.ReadLine();
         }
 
     }
@@ -33,6 +46,7 @@ namespace C0727128
                 GPA = bGPA;
                 Attendance = cAttendance;
             }
+           
     }
 }    
 
